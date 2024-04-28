ba6301258a5e2a8ddc044f3d6e92d41e5fb583c8
ilanae03 <ilan.eschiezaroarnaut@gmail.com>
2024-04-28 20:15:28 -0300

    inclusão do Ex1 sobre Pokemon e Ex2 do IBGE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { RotaComponent } from './components/rota/rota.component';
 import { ClientDetailComponent } from './components/client-detail/client-detail.component';
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { TarefasComponent } from './components/tarefas/tarefas.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { IbgeComponent } from './components/ibge/ibge.component';
 
 export const routes: Routes = [
 
@@ -12,6 +14,8 @@ export const routes: Routes = [
     {path: 'cliente/:id', component: ClientDetailComponent},
     {path: 'cliente', component: ClienteComponent},
     {path: 'tarefa', component: TarefasComponent},
+    {path: 'pokemon', component: PokemonComponent},
+    {path: 'ibge', component: IbgeComponent},
     {path: '**', component: HomeComponent}
 
 ];
